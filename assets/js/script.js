aa927eb463d07acdf7fd1f506e3d76a31ee019a4
Amit Bisht <bishtakb69@gmail.com>
2024-09-24 08:38:42 +0530

    changes done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
-    
+    $('#headerul a.nav-link').on('click', function(event) {
+        if (this.hash !== "") {
+          event.preventDefault();
+          var hash = this.hash;
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800);  // Adjust the speed (in milliseconds) as needed
+        }
+      });
+      
 });
